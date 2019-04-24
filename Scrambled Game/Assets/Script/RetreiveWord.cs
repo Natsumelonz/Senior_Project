4769e25a6c188b230e31a0011959faf258c290e0
Melonnnnn <natsumelonz@gmail.com>
2019-04-24 11:14:08 +0700

    [ADD] RestClient , RetriveDataFormDB , TestRetrieve in Scrabble

---
+++ Scrambled Game/Assets/Script/RetreiveWord.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class RetreiveWord
+{
+    public int word_id;
+    public string wordname_JP;
+    public string wordname_romanji;
+    public string word_meaning;
+    public int word_syllable;
+
+    public override string ToString()
+    {
+        return JsonUtility.ToJson(this,true);
+    }
+}
