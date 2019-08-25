a93f14127fd4bc85d6c9965efa77e22570164919
chanantorn <napza_1@hotmail.com>
2019-08-25 10:08:16 +0700

    [ADD] Clone Choice

--- Yomimasu_Game/Assets/Scripts/Chapter_1.cs
+++ Yomimasu_Game/Assets/Scripts/Chapter_1.cs
@@ -19,7 +19,20 @@ public class Chapter_1 : MonoBehaviour
     public Text _event;
     public Text speaker;
     public Text textDisplays;
-    
+
+    public GameObject ChapterCanvas;
+    public CharObjectOfChapter prefab;
+
+    //ไว้เก็บbutton ที่clone มา
+    public Transform container;
+    public float space;
+    public float lerpSpeed = 5;
+    List<CharObjectOfChapter> charObjectOfChapter = new List<CharObjectOfChapter>();
+
+    CharObjectOfChapter firstSelected;
+
+    public static Chapter_1 main;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -72,6 +85,56 @@ public class Chapter_1 : MonoBehaviour
         }
     }
 
+    void RepositionObject()
+    {
+
+        if (charObjectOfChapter.Count == 0)
+        {
+            return;
+        }
+        float center = (charObjectOfChapter.Count - 1f) / 2;
+        for (int i = 0; i < charObjectOfChapter.Count; i++)
+        {
+            charObjectOfChapter[i].rectTransform.anchoredPosition
+                = Vector2.Lerp(charObjectOfChapter[i].rectTransform.anchoredPosition,
+                 new Vector2((i - center) * space, 0), lerpSpeed * Time.deltaTime);
+            charObjectOfChapter[i].index = i;
+        }
+    }
+    void Awake()
+    {
+        main = this;
+    }
+
+    void CloneChoice()
+    {
+        charObjectOfChapter.Clear();
+        foreach (Transform child in container)
+        {
+            Destroy(child.gameObject);
+        }
+
+        string[] sentence = sentences[index].ToString().Split();
+        foreach (string s in sentence)
+        {
+            CharObjectOfChapter clone = Instantiate(prefab.gameObject).GetComponent<CharObjectOfChapter>();
+            clone.transform.SetParent(container);
+            charObjectOfChapter.Add(clone.Init(s));
+        }
+    }
+    public void Select(CharObjectOfChapter charObjectOfChapter)
+    {
+        if (firstSelected)
+        {
+            firstSelected.Select();
+            charObjectOfChapter.Select();
+
+        }
+        else
+        {
+            firstSelected = charObjectOfChapter;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -79,6 +142,8 @@ public class Chapter_1 : MonoBehaviour
         if (textDisplays.text == sentences[index])
         {
             continueButton.SetActive(true);
+            CloneChoice();
+            RepositionObject();
         }
 
         if (DialogManager.dialog[index].script_role == "Teacher")
@@ -90,5 +155,6 @@ public class Chapter_1 : MonoBehaviour
 
             teacherPic.SetActive(false);
         }
+   
     }
 }
---
+++ Yomimasu_Game/Assets/Scripts/CharObjectOfChapter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharObjectOfChapter : MonoBehaviour
+{
+    public string sentence;
+    public Text text;
+    public Image image;
+    public RectTransform rectTransform;
+    public int index;
+
+    [Header("Appearance")]
+    public Color normalColor;
+    public Color selectedColor;
+
+    bool isSelected = false;
+
+
+    public CharObjectOfChapter Init(string c)
+    {
+        sentence = c;
+        text.text = c.ToString();
+        gameObject.SetActive(true);
+        return this;
+    }
+
+    public void Select()
+    {
+        isSelected = !isSelected;
+        image.color = isSelected ? selectedColor : normalColor;
+
+    }
+    // Start is called before the first frame update 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+
+    }
+}
