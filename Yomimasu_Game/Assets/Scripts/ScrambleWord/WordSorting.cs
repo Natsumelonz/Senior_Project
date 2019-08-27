aa4a6107908e8ed90a958de5a5c0d102d5352ff8
chanantorn <napza_1@hotmail.com>
2019-08-27 15:04:01 +0700

    [EDIT] Swap Romanji & Japan

--- Yomimasu_Game/Assets/Scripts/ScrambleWord/WordSorting.cs
+++ Yomimasu_Game/Assets/Scripts/ScrambleWord/WordSorting.cs
@@ -160,7 +160,8 @@ public class WordSorting : MonoBehaviour
         yield return new WaitForSeconds(time);
         for (int i = 0; i < 20; i++)
         {
-            words[i].word = WordManager.words[numIndexList[i]].wordname_romanji;
+            //words[i].word = WordManager.words[numIndexList[i]].wordname_romanji;
+            words[i].word = WordManager.words[numIndexList[i]].wordname_JP;
         }
 
         if (!_initRAD)
@@ -216,8 +217,9 @@ public class WordSorting : MonoBehaviour
         }
 
         if(currentWord <= numIndexList.Count)
-        { 
-            result.textJapan.text = WordManager.words[numIndexList[currentWord]].wordname_JP.ToString();
+        {
+            //result.textJapan.text = WordManager.words[numIndexList[currentWord]].wordname_JP.ToString();
+            result.textJapan.text = WordManager.words[numIndexList[currentWord]].wordname_romanji.ToString();
             result.textMean.text = WordManager.words[numIndexList[currentWord]].word_meaning.ToString();
         }
         //แสดงตามindex 
