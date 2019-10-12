dbf5cc1441697b70399714a747fa5d2e3862cf77
BounceGem <mildsadz@gmail.com>
2019-10-13 03:14:16 +0700

    [ADD] some loading screen

--- Yomimasu_Game/Assets/Scripts/Manager/Preloader.cs
+++ Yomimasu_Game/Assets/Scripts/Manager/Preloader.cs
@@ -2,25 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Preloader : MonoBehaviour
 {
     private GameObject Manager;
+    private AsyncOperation async;
+
+    public GameObject loading;
+    public Slider slider;
 
     void Start()
     {
         Manager = GameObject.Find("GameData").gameObject;
+
+        StartCoroutine(LoadingScreen());
     }
 
-    void Update()
+    public IEnumerator LoadingScreen()
     {
-        if (Manager.GetComponent<AlphabetManager>().alphabetsHR.Count != 0 && Manager.GetComponent<AlphabetManager>().alphabetsKT.Count != 0 
-            && Manager.GetComponent<WordManager>().words.Count != 0 && Manager.GetComponent<LeaderBoardManager>().leaderBoard1.Count != 0 
+        loading.SetActive(true);
+        async = SceneManager.LoadSceneAsync("MainMenu");
+        async.allowSceneActivation = false;
+
+        while (!async.isDone)
+        {
+            slider.value = async.progress;
+            yield return new WaitForEndOfFrame();
+
+            if (async.progress == 0.9f
+            && Manager.GetComponent<AlphabetManager>().alphabetsHR.Count != 0 && Manager.GetComponent<AlphabetManager>().alphabetsKT.Count != 0
+            && Manager.GetComponent<WordManager>().words.Count != 0 && Manager.GetComponent<LeaderBoardManager>().leaderBoard1.Count != 0
             && Manager.GetComponent<LeaderBoardManager>().leaderBoard2.Count != 0 && Manager.GetComponent<UserManager>().user.Name != ""
             && Manager.GetComponent<ContentManager>().chapter1_info.chap_id != 0 && Manager.GetComponent<ContentManager>().chapter2_info.chap_id != 0
             && !UserManager.fistTime)
-        {
-            SceneManager.LoadScene("MainMenu");
+            {
+
+                yield return new WaitForSeconds(1f);
+                slider.value = 1f;
+                async.allowSceneActivation = true;
+            }
         }
     }
 }
