ec37538359c6371352dcc0e8853a746a0798f05f
Melonnnnn <natsumelonz@gmail.com>
2019-10-13 09:37:06 +0700

    [ADD] Vuetify css , Vue-router , V-icon

--- Yomimasu_Webapp/yomimasu/src/main.js
+++ Yomimasu_Webapp/yomimasu/src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+/*Path*/
+//import path eg. import User from './Users.vue';
+
+const routes = [
+  //path pattern eg.{ path: '/users' , component: User }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
   vuetify,
